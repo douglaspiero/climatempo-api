7f2107d6c1a3b1a373d16017bf467d743c657805
Douglas Piero <45581314+douglaspiero@users.noreply.github.com>
2022-01-13 21:28:02 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var localip = require('local-ip');
-var iface = 'wlan0';
- 
-localip(iface, function(err, res) {
-  if (err) {
-    console.log(err + 'I have no idea what my local ip is.');
-  }
-  console.log('My local ip address on ' + iface + ' is ' + res);
-});
